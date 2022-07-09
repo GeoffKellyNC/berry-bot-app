b088b65dd02649855f5475c37885a0ccef902346
Geoff Kelly <65321097+GeoffKellyNC@users.noreply.github.com>
2022-07-08 23:49:08 -0400

    removed config data

--- backend/index.js
+++ backend/index.js
@@ -25,7 +25,7 @@ app.use( bodyParser.json() );       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
   extended: true
 })); 
-
+ 
 
 // GET 
 app.use('/getTarget', routerGetTarget); // route to get target 
